65fc513d9ef5abffecfac1c43a91c07be7560df1
ed0176 <ed0176@Apple-T-AE8772.local>
2022-02-21 18:27:21 +0800

    db

--- common/db/mysql.go
+++ common/db/mysql.go
@@ -1,6 +1,5 @@
 package db
 
-/*
 import (
 	"fmt"
 
@@ -10,34 +9,36 @@ import (
 	"gorm.io/gorm"
 )
 
+var MainSession *MySQL
+
+func init() {
+	var err error
+	MainSession, err = NewMySQL("root", "", "127.0.0.1", "aha")
+	if err != nil {
+		panic("sql init: " + err.Error())
+	}
+}
+
 const (
 	//DIALECT
 	DIALECT = "mysql"
 )
 
 //Maria ...
-type Maria struct {
-	db *gorm.DB
+type MySQL struct {
+	*gorm.DB
 }
 
 //NewMaria instance Maria struct
 //initialize a new db connection
 //db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
 //host ip:port
-func NewMaria(user, password, host, dbName string) (*Maria, error) {
-	mysql.Open("dst")
+func NewMySQL(user, password, host, dbName string) (*MySQL, error) {
 	args := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&loc=UTC&timeout=30s", user, password, host, dbName)
-
 	//"%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&loc=UTC&timeout=30s"
-	db, err := gorm.Open(DIALECT, args)
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
 
-	return &Maria{
-		db: db,
+	return &MySQL{
+		db,
 	}, err
 }
-
-//Close close current db connection.
-func (m *Maria) Close() {
-	m.db.Close()
-}
-*/
